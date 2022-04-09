1a5ef2bc6453a711bf891543103956f4c8543985
Rabby <fajla.rabby008@gmail.com>
2022-04-09 19:18:24 +0600

    react router auth

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -3,16 +3,16 @@ import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import app from '../../firebase.init';
 
-const  auth = getAuth(app);
+const auth = getAuth(app);
 
-const Home = () => {
+const Products = () => {
     const [user] = useAuthState(auth);
     return (
         <div>
-            <h2>This is Home</h2>
-            <p>current user is : {user? user.displayName: 'No body' }</p>
+                <h2>Knoc Knock Who is there?</h2>
+                <h5>{user? user.displayName : 'voooooooooooooot'}</h5>
         </div>
     );
 };
 
-export default Home;
+export default Products;
